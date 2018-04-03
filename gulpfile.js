303ee9bfd4028654f2a67a451b5b8b0ac855c4b7
JessicaMP <jessica95.mp@gmail.com>
2018-04-03 11:51:42 -0500

    gulp entorno

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var minifycss = require('gulp-minify-css')
+var sass = require('gulp-sass');
+
+gulp.task('js', function (params) {
+    return gulp.src('src/js/*.js')
+    .pipe(concat('app.min.js'))
+    .pipe(babel({ presets: ['env']}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'))
+})
+
+gulp.task('css', function() {
+    return gulp.src(['src/css/*.css', 'src/sass/*.scss'])
+    .pipe(sass())
+    .pipe(concat('main.min.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css/'))
+})
+
+// gulp.task('default', ['css', 'js'])
+
+gulp.task('watch', function(){
+    gulp.watch(['src/css/*.css', 'src/sass/*.scss'], ['css']);
+    gulp.watch('src/js/*.js', ['js']);
+})
---
+++ src/js/operation.js
@@ -0,0 +1,6 @@
+//Funcion suma
+var average = function(num1, num2) {
+    return(num1 + num2) /2;
+}
+
+console.log(average(3, 5));
